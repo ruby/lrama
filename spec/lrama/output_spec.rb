cf3dc6a5d0debe5a4088cc18995800cc6c4e935b
yui-knk <spiketeika@gmail.com>
2023-03-18 16:45:14 +0900

    Add test for Lrama::Output

---
+++ spec/lrama/output_spec.rb
@@ -0,0 +1,34 @@
+require "stringio"
+
+RSpec.describe Lrama::Output do
+  let(:output) {
+    Lrama::Output.new(
+      out: out,
+      output_file_path: "y.tab.c",
+      template_name: "bison/yacc.c",
+      grammar_file_path: "parse.tmp.y",
+      header_out: header_out,
+      header_file_path: "y.tab.h",
+      context: context,
+      grammar: grammar,      
+    )
+  }
+  let(:out) { StringIO.new }
+  let(:header_out) { StringIO.new }
+  let(:context) { double("context") }
+  let(:grammar) { double("grammar") }
+
+  describe "#parse_param" do
+    it "returns declaration of parse param without braces" do
+      allow(grammar).to receive(:parse_param).and_return("{struct parser_params *p}")
+      expect(output.parse_param).to eq("struct parser_params *p")
+    end
+  end
+
+  describe "#user_formals" do
+    it "returns declaration of parse param without braces with a leading comma" do
+      allow(grammar).to receive(:parse_param).and_return("{struct parser_params *p}")
+      expect(output.user_formals).to eq(", struct parser_params *p")
+    end
+  end
+end
