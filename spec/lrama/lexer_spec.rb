99c937b60d18c613f9ea28fc9f38bc7aaa765949
TSUYUSATO Kitsune <make.just.on@gmail.com>
2023-05-16 17:56:18 +0900

    Support `//` style comment

--- spec/lrama/lexer_spec.rb
+++ spec/lrama/lexer_spec.rb
@@ -387,11 +387,11 @@ program: stmt ;
 
 stmt: tBODY
         {
-          int i = 1; /* @ */
-          int j = 1; /* $ */
-          int k = 1; /* @1 */
-          int l = 1; /* $$ */
-          int m = 1; /* $2 */
+          int i = 1; /* @ */ // @
+          int j = 1; /* $ */ // $
+          int k = 1; /* @1 */ // @1
+          int l = 1; /* $$ */ // $$
+          int m = 1; /* $2 */ // $2
         }
     ;
 %%
@@ -403,11 +403,11 @@ stmt: tBODY
 
         expected = <<-CODE.chomp
 {
-          int i = 1; /* @ */
-          int j = 1; /* $ */
-          int k = 1; /* @1 */
-          int l = 1; /* $$ */
-          int m = 1; /* $2 */
+          int i = 1; /* @ */ // @
+          int j = 1; /* $ */ // $
+          int k = 1; /* @1 */ // @1
+          int l = 1; /* $$ */ // $$
+          int m = 1; /* $2 */ // $2
         }
         CODE
 
