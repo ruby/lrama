e052c0af2b75d985dbc75a9d95e0d934f25db15c
alitaso345 <alice.maru345@gmail.com>
2023-06-22 22:25:50 +0900

    Add tests for stdin mode
    There are the tests corresponding to the following implementation https://github.com/ruby/lrama/pull/8

--- spec/lrama/command_spec.rb
+++ spec/lrama/command_spec.rb
@@ -1,4 +1,41 @@
 RSpec.describe Lrama::Command do
+  describe "#run" do
+    let(:command) { Lrama::Command.new }
+
+    describe "a grammar file is specified " do
+      it "ends successfully" do
+        expect(command.run([fixture_path("command/basic.y")])).to be_nil
+      end
+    end
+
+    describe "STDIN mode and a grammar file is specified" do
+      it "ends successfully" do
+        File.open(fixture_path("command/basic.y")) do |f|
+          allow(STDIN).to receive(:read).and_return(f)
+          expect(command.run(["-", "test.y"])).to be_nil
+        end
+      end
+    end
+
+    describe "invalid argv" do
+      describe "a grammar file isn't specified" do
+        it "returns stderr" do
+          expect{ command.run([]) }.to raise_error(SystemExit) do |e|
+            expect(e.message).to eq("File should be specified\n")
+          end
+        end
+      end
+
+      describe "STDIN mode, but a grammar file isn't specified" do
+        it "returns stderr" do
+          expect{ command.run(["-"]) }.to raise_error(SystemExit) do |e|
+            expect(e.message).to eq("File name for STDIN should be specified\n")
+          end
+        end
+      end
+    end
+  end
+
   describe "#validate_report" do
     let(:command) { Lrama::Command.new }
 
