2fb25820ef0c7aa3cf7f6f1a4a0486234228d3b9
yui-knk <spiketeika@gmail.com>
2023-08-06 16:48:32 +0900

    Add parameters configured integration test

--- spec/lrama/integration_spec.rb
+++ spec/lrama/integration_spec.rb
@@ -3,14 +3,36 @@ require "open3"
 
 RSpec.describe "integration" do
   module IntegrationHelper
-    def test_rules(rules, input, expected, command_args: [], debug: false)
-      cases = input.each_with_index.map do |(token, union, semantic_value), i|
+    def test_grammar(grammar, expected, command_args: [])
+      Tempfile.create(%w[test .y]) do |f|
+        f << grammar
+        f.close
+        c_path = File.dirname(f.path) + "/test.c"
+        obj_path = File.dirname(f.path) + "/test"
+
+        Lrama::Command.new(%W[-d -o #{c_path}] + command_args + %W[#{f.path}]).run
+
+        `gcc -Wall #{c_path} -o #{obj_path}`
+
+        result = Open3.popen3(obj_path) do |stdin, stdout, stderr, wait_thr|
+          stdout.read
+        end
+
+        expect(result).to eq(expected)
+      end
+    end
+
+    def generate_lexer_body(input)
+      input.each_with_index.map do |(token, union, semantic_value), i|
         str = ""
         str << "    case #{i}:\n"
         str << "        yylval->#{union} = #{semantic_value};\n" if union && semantic_value
         str << "        return #{token};\n"
       end.join("\n")
+    end
 
+    def test_rules(rules, input, expected, command_args: [], debug: false)
+      cases = generate_lexer_body(input)
       yydebug_macro = ''
       yydebug = ''
 
@@ -59,22 +81,7 @@ int main() {
 
       Grammar
 
-      Tempfile.create(%w[test .y]) do |f|
-        f << grammar
-        f.close
-        c_path = File.dirname(f.path) + "/test.c"
-        obj_path = File.dirname(f.path) + "/test"
-
-        Lrama::Command.new(%W[-d -o #{c_path}] + command_args + %W[#{f.path}]).run
-
-        `gcc -Wall #{c_path} -o #{obj_path}`
-
-        result = Open3.popen3(obj_path) do |stdin, stdout, stderr, wait_thr|
-          stdout.read
-        end
-
-        expect(result).to eq(expected)
-      end
+      test_grammar(grammar, expected, command_args: command_args)
     end
   end
 
@@ -117,6 +124,83 @@ int main() {
 
   %%
       Rules
+    end
+  end
+
+  describe "YYDEBUG, %lex-param, %parse-param, error_recovery option are enabled" do
+    it "returns 9 for '(1+2)*3'" do
+      # (1+2)*3 #=> 9
+      input = [
+        %w['('],
+        %w[NUM val 1],
+        %w['+'],
+        %w[NUM val 2],
+        %w[')'],
+        %w['*'],
+        %w[NUM val 3]
+      ]
+      cases = generate_lexer_body(input)
+
+      test_grammar(<<~Grammar, "=> 9", command_args: %W[-e])
+  %{
+  #define YYDEBUG 1
+  #include <stdio.h>
+
+  #define YY_LOCATION_PRINT(File, loc, p) ((void) 0)
+
+  #include "test.h"
+
+  static int yylex(YYSTYPE *yylval, YYLTYPE *loc, int lex_param);
+  static int yyerror(YYLTYPE *loc, int parse_param, const char *str);
+  %}
+
+  %lex-param {int parse_param}
+  %parse-param {int parse_param}
+
+  %union {
+      int val;
+  }
+  %token <val> NUM
+  %type <val> expr
+  %left '+' '-'
+  %left '*' '/'
+
+  %%
+
+  program : { (void)yynerrs; }
+       | expr { printf("=> %d", $1); }
+       ;
+  expr : NUM
+       | expr '+' expr { $$ = $1 + $3; }
+       | expr '-' expr { $$ = $1 - $3; }
+       | expr '*' expr { $$ = $1 * $3; }
+       | expr '/' expr { $$ = $1 / $3; }
+       | '(' expr ')'  { $$ = $2; }
+       ;
+
+  %%
+
+  int c = 0;
+
+  static int yylex(YYSTYPE *yylval, YYLTYPE *loc, int lex_param) {
+      switch (c++) {
+  #{cases}
+      default:
+          // End of Input
+          return -1;
+      }
+  }
+
+  static int yyerror(YYLTYPE *loc, int parse_param, const char *str) {
+      fprintf(stderr, "parse error: %s\\n", str);
+      return 0;
+  }
+
+  int main() {
+      yyparse(0);
+      return 0;
+  }
+      Grammar
     end
   end
 
