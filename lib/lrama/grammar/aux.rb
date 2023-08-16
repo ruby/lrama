c35dc0e8e6e3a372c2b1038c0f4236d62e88aef6
yui-knk <spiketeika@gmail.com>
2023-08-16 21:51:16 +0900

    Rename Aux to Auxiliary
    Can not use "aux" for file name in Windows.

--- lib/lrama/grammar/aux.rb
+++
@@ -1,7 +0,0 @@
-module Lrama
-  class Grammar
-    # Grammar file information not used by States but by Output
-    class Aux < Struct.new(:prologue_first_lineno, :prologue, :epilogue_first_lineno, :epilogue, keyword_init: true)
-    end
-  end
-end
