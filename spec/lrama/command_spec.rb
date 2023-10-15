23886d97805c64abb35115ea742d71d8867e91c2
ydah <13041216+ydah@users.noreply.github.com>
2023-10-16 00:55:20 +0900

    Use context instead of describe

--- spec/lrama/command_spec.rb
+++ spec/lrama/command_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Lrama::Command do
     let(:outfile) { File.join(Dir.tmpdir, "parse.c") }
     let(:o_option) { ["-o", "#{outfile}"] }
 
-    describe "a grammar file is specified" do
+    context "when grammar file is specified" do
       it "ends successfully" do
         command = Lrama::Command.new
         expect(command.run(o_option + [fixture_path("command/basic.y")])).to be_nil
       end
     end
 
-    describe "STDIN mode and a grammar file is specified" do
+    context "when STDIN mode and a grammar file is specified" do
       it "ends successfully" do
         File.open(fixture_path("command/basic.y")) do |f|
           allow(STDIN).to receive(:read).and_return(f.read)
