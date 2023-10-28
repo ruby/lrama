6541c7f1205b3b903a621d7286a22d38ece8891d
yui-knk <spiketeika@gmail.com>
2023-10-28 14:41:56 +0900

    Token type of C_DECLARATION is always `Token::User_code`

--- spec/lrama/grammar/code_spec.rb
+++ spec/lrama/grammar/code_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Lrama::Grammar::Code do
   let(:token_class) { Lrama::Lexer::Token }
   let(:user_code_token) { token_class.new(type: token_class::User_code, s_value: "{ code 1 }") }
-  let(:initial_act_token) { token_class.new(type: token_class::P_initial_action, s_value: "%initial-action") }
+  let(:initial_act_token) { token_class.new(type: token_class::User_code, s_value: "%initial-action") }
 
   describe "#translated_code" do
     context "when the code type is :user_code" do
