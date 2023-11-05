1b4f3c418985bfa95c76ab2235bf6f3fa70ecfdc
yui-knk <spiketeika@gmail.com>
2023-11-05 17:59:16 +0900

    Update comments

--- lib/lrama/grammar/reference.rb
+++ lib/lrama/grammar/reference.rb
@@ -1,8 +1,8 @@
-# type: :dollar or :at
-# ex_tag: "$<tag>1" (Optional)
-
 module Lrama
   class Grammar
+    # type: :dollar or :at
+    # value: Integer (e.g. $1) or String (e.g. $$, $foo, $expr.right)
+    # ex_tag: "$<tag>1" (Optional)
     class Reference < Struct.new(:type, :value, :ex_tag, :first_column, :last_column, :referring_symbol, :position_in_rhs, keyword_init: true)
       def tag
         if ex_tag
