ef0e151924979788230ff339d2f853430638d1d6
ydah <13041216+ydah@users.noreply.github.com>
2023-11-22 22:53:48 +0900

    Fix separated_list expansion
    Before
    ```
    program: separated_list(',', number)
    ↓
    program: separated_list_number
    separated_list_number: ε
    separated_list_number: number
    separated_list_number: separated_list_number ',' number
    ```
    
    After
    ```
    program: separated_list(',', number)
    ↓
    program: separated_list
    separated_list_number: ε
    separated_list_number: separated_nonempty_list_number
    separated_nonempty_list_number: number
    separated_nonempty_list_number: separated_nonempty_list_number ',' number
    ```

--- lib/lrama/grammar/parameterizing_rules/builder/separated_list.rb
+++ lib/lrama/grammar/parameterizing_rules/builder/separated_list.rb
@@ -16,17 +16,20 @@ module Lrama
           #
           # program: separated_list_number
           # separated_list_number: ε
-          # separated_list_number: number
-          # separated_list_number: separated_list_number ',' number
+          # separated_list_number: separated_nonempty_list_number
+          # separated_nonempty_list_number: number
+          # separated_nonempty_list_number: separated_nonempty_list_number ',' number
           def build
             validate_argument_number!
 
             rules = []
             separated_list_token = Lrama::Lexer::Token::Ident.new(s_value: "separated_list_#{@token.s_value}")
+            separated_nonempty_list_token = Lrama::Lexer::Token::Ident.new(s_value: "separated_nonempty_list_#{@token.s_value}")
             rules << Rule.new(id: @rule_counter.increment, _lhs: @lhs, _rhs: [separated_list_token], token_code: @user_code, precedence_sym: @precedence_sym, lineno: @line)
             rules << Rule.new(id: @rule_counter.increment, _lhs: separated_list_token, _rhs: [], token_code: @user_code, precedence_sym: @precedence_sym, lineno: @line)
-            rules << Rule.new(id: @rule_counter.increment, _lhs: separated_list_token, _rhs: [@token], token_code: @user_code, precedence_sym: @precedence_sym, lineno: @line)
-            rules << Rule.new(id: @rule_counter.increment, _lhs: separated_list_token, _rhs: [separated_list_token, @separator, @token], token_code: @user_code, precedence_sym: @precedence_sym, lineno: @line)
+            rules << Rule.new(id: @rule_counter.increment, _lhs: separated_list_token, _rhs: [separated_nonempty_list_token], token_code: @user_code, precedence_sym: @precedence_sym, lineno: @line)
+            rules << Rule.new(id: @rule_counter.increment, _lhs: separated_nonempty_list_token, _rhs: [@token], token_code: @user_code, precedence_sym: @precedence_sym, lineno: @line)
+            rules << Rule.new(id: @rule_counter.increment, _lhs: separated_nonempty_list_token, _rhs: [separated_nonempty_list_token, @separator, @token], token_code: @user_code, precedence_sym: @precedence_sym, lineno: @line)
             rules
           end
         end
