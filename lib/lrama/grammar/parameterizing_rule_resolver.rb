350e4ad407908cf706083a187395eaacc3eed775
yui-knk <spiketeika@gmail.com>
2023-12-24 13:29:03 +0900

    Extract rule_builders selection logic into private method

--- lib/lrama/grammar/parameterizing_rule_resolver.rb
+++ lib/lrama/grammar/parameterizing_rule_resolver.rb
@@ -10,14 +10,20 @@ module Lrama
       end
 
       def defined?(name)
-        @parameterizing_rule_builders.any? { |builder| builder.name == name }
+        !rule_builders(name).empty?
       end
 
       def build_rules(token, rule_counter, lhs_tag, line)
-        builder = @parameterizing_rule_builders.select { |b| b.name == token.s_value }.last
+        builder = rule_builders(token.s_value).last
         raise "Unknown parameterizing rule #{token.s_value} at line #{token.line}" unless builder
 
         builder.build_rules(token, token.args, rule_counter, lhs_tag, line, @parameterizing_rule_builders)
+      end
+
+      private
+
+      def rule_builders(name)
+        @parameterizing_rule_builders.select { |builder| builder.name == name }
       end
     end
   end
