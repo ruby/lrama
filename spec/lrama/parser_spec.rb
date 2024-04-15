2435ee6bd918c456ba390ae7555c2af2c939263d
ydah <t.yudai92@gmail.com>
2024-04-15 18:24:23 +0900

    Add test code for index adjustment and $n reverse

--- spec/lrama/parser_spec.rb
+++ spec/lrama/parser_spec.rb
@@ -2450,6 +2450,70 @@ RSpec.describe Lrama::Parser do
           ])
         end
       end
+
+      context 'when inline with resolve reverse index' do
+        let(:path) { "inlining/resolve_index_reverse.y" }
+
+        it "expands inlining rules" do
+          expect(grammar.nterms.sort_by(&:number)).to match_symbols([
+            Sym.new(id: T::Ident.new(s_value: "$accept"), alias_name: nil, number: 6, tag: nil, term: false, token_id: 0, nullable: false),
+            Sym.new(id: T::Ident.new(s_value: "expression"), alias_name: nil, number: 7, tag:  T::Tag.new(s_value: "<i>"), term: false, token_id: 1, nullable: false),
+          ])
+
+          expect(grammar.rules).to eq([
+            Rule.new(
+              id: 2,
+              lhs: grammar.find_symbol_by_s_value!("$accept"),
+              rhs: [
+                grammar.find_symbol_by_s_value!("expression"),
+                grammar.find_symbol_by_s_value!("YYEOF"),
+              ],
+              token_code: nil,
+              nullable: false,
+              precedence_sym: grammar.find_symbol_by_s_value!("YYEOF"),
+              lineno: 24,
+            ),
+            Rule.new(
+              id: 3,
+              lhs: grammar.find_symbol_by_s_value!("expression"),
+              rhs: [
+                grammar.find_symbol_by_s_value!("NUM"),
+              ],
+              token_code: nil,
+              nullable: false,
+              precedence_sym: grammar.find_symbol_by_s_value!("NUM"),
+              lineno: 24,
+            ),
+            Rule.new(
+              id: 4,
+              lhs: grammar.find_symbol_by_s_value!("expression"),
+              rhs: [
+                grammar.find_symbol_by_s_value!("expression"),
+                grammar.find_symbol_by_s_value!("'+'"),
+                grammar.find_symbol_by_s_value!("expression"),
+              ],
+              token_code: T::UserCode.new(s_value: " $$ = $3  +  $1; "),
+              nullable: false,
+              precedence_sym: grammar.find_symbol_by_s_value!("'+'"),
+              lineno: 25,
+            ),
+            Rule.new(
+              id: 5,
+              lhs: grammar.find_symbol_by_s_value!("expression"),
+              rhs: [
+                grammar.find_symbol_by_s_value!("expression"),
+                grammar.find_symbol_by_s_value!("'+'"),
+                grammar.find_symbol_by_s_value!("'='"),
+                grammar.find_symbol_by_s_value!("expression"),
+              ],
+              token_code: T::UserCode.new(s_value: " $$ = $4  +=  $1; "),
+              nullable: false,
+              precedence_sym: grammar.find_symbol_by_s_value!("'='"),
+              lineno: 25,
+            ),
+          ])
+        end
+      end
     end
 
     it "; for rules is optional" do
