14d1bc8afde7c6fe6943e541b2b90e92298a76a8
ydah <t.yudai92@gmail.com>
2025-01-01 11:39:01 +0900

    Do not include mid-rule actions when tracing rules
    This PR, midrule actions should not be included when tracing rules.
    
    Before:
    ```
    ❯ exe/lrama --trace=rules ./spec/fixtures/common/basic.y
    Grammar rules:
    $accept -> program EOI
    program -> class
    program -> '+' strings_1
    program -> '-' strings_2
    class -> keyword_class tSTRING keyword_end
    $@1 -> ε
    $@2 -> ε
    class -> keyword_class $@1 tSTRING '!' keyword_end $@2
    $@3 -> ε
    $@4 -> ε
    class -> keyword_class $@3 tSTRING '?' keyword_end $@4
    strings_1 -> string_1
    strings_2 -> string_1
    strings_2 -> string_2
    string_1 -> string
    string_2 -> string '+'
    string -> tSTRING
    unused -> tNUMBER
    ```
    
    After:
    ```
    ❯ exe/lrama --trace=rules ./spec/fixtures/common/basic.y
    Grammar rules:
    $accept -> program EOI
    program -> class
    program -> '+' strings_1
    program -> '-' strings_2
    class -> keyword_class tSTRING keyword_end
    class -> keyword_class tSTRING '!' keyword_end
    class -> keyword_class tSTRING '?' keyword_end
    strings_1 -> string_1
    strings_2 -> string_1
    strings_2 -> string_2
    string_1 -> string
    string_2 -> string '+'
    string -> tSTRING
    unused -> tNUMBER
    ```

--- lib/lrama/trace_reporter.rb
+++ lib/lrama/trace_reporter.rb
@@ -24,7 +24,9 @@ module Lrama
     # @rbs () -> void
     def report_rules
       puts "Grammar rules:"
-      @grammar.rules.each { |rule| puts rule.display_name }
+      @grammar.rules.each do |rule|
+        puts rule.display_name_without_action if rule.lhs.first_set.any?
+      end
     end
 
     # @rbs () -> void
