8b3356bffca92380d20456255ab35b686e09a982
yui-knk <spiketeika@gmail.com>
2025-01-20 11:52:52 +0900

    Migrate digraph.rb rbs to rbs-inline

--- lib/lrama/digraph.rb
+++ lib/lrama/digraph.rb
@@ -1,23 +1,40 @@
+# rbs_inline: enabled
 # frozen_string_literal: true
 
 module Lrama
   # Algorithm Digraph of https://dl.acm.org/doi/pdf/10.1145/69622.357187 (P. 625)
   class Digraph
+    # @rbs sets: String
+    # @rbs relation: Hash[Integer, Array[Integer]]
+    # @rbs base_function: Hash[Integer, Integer]
+    # @rbs return: void
     def initialize(sets, relation, base_function)
       # X in the paper
+      # @rbs @sets: Array[Integer]
       @sets = sets
+
       # R in the paper
+      # @rbs @relation: Hash[Integer, Array[Integer]]
       @relation = relation
+
       # F' in the paper
+      # @rbs @base_function: Hash[Integer, Integer]
       @base_function = base_function
+
       # S in the paper
+      # @rbs @stack: Array[Integer]
       @stack = []
+
       # N in the paper
+      # @rbs @h: Hash[Integer, (Integer|Float)?]
       @h = Hash.new(0)
+
       # F in the paper
+      # @rbs @result: Hash[Integer, Integer]
       @result = {}
     end
 
+    # @rbs () -> Hash[Integer, Integer]
     def compute
       @sets.each do |x|
         next if @h[x] != 0
@@ -29,6 +46,7 @@ module Lrama
 
     private
 
+    # @rbs (Integer x) -> void
     def traverse(x)
       @stack.push(x)
       d = @stack.count
