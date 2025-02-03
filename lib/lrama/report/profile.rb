44d383cc7ab91c3a3d988fadfd52449da46e132b
yui-knk <spiketeika@gmail.com>
2025-02-03 13:03:14 +0900

    Move `Lrama::Report::Profile` to `Lrama::Report::Profile::CallStack`

--- lib/lrama/report/profile.rb
+++ lib/lrama/report/profile.rb
@@ -1,32 +1,3 @@
-# rbs_inline: enabled
 # frozen_string_literal: true
 
-module Lrama
-  class Report
-    module Profile
-      # See "Call-stack Profiling Lrama" in README.md for how to use.
-      #
-      # @rbs enabled: bool
-      # @rbs &: -> void
-      # @rbs return: StackProf::result | void
-      def self.report_profile(enabled)
-        if enabled && require_stackprof
-          StackProf.run(mode: :cpu, raw: true, out: 'tmp/stackprof-cpu-myapp.dump') do
-            yield
-          end
-        else
-          yield
-        end
-      end
-
-      # @rbs return: bool
-      def self.require_stackprof
-        require "stackprof"
-        true
-      rescue LoadError
-        warn "stackprof is not installed. Please run `bundle install`."
-        false
-      end
-    end
-  end
-end
+require_relative 'profile/call_stack'
