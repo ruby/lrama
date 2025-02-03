d8214e73272f97d3f47b63f507ffa261b2833dd7
yui-knk <spiketeika@gmail.com>
2025-02-03 11:26:54 +0900

    Extract `require "stackprof"` to a method

--- lib/lrama/report/profile.rb
+++ lib/lrama/report/profile.rb
@@ -5,11 +5,17 @@ module Lrama
     module Profile
       # See "Profiling Lrama" in README.md for how to use.
       def self.report_profile
-        require "stackprof"
+        require_stackprof
 
         StackProf.run(mode: :cpu, raw: true, out: 'tmp/stackprof-cpu-myapp.dump') do
           yield
         end
+      end
+
+      def self.require_stackprof
+        require "stackprof"
+      rescue LoadError
+        warn "stackprof is not installed. Please run `bundle install`."
       end
     end
   end
