7c36f51b29a75fc4ac83423b8d7e110758440c72
yui-knk <spiketeika@gmail.com>
2025-02-03 12:56:39 +0900

    Use rbs_inline for lib/lrama/report/profile.rb

--- lib/lrama/report/profile.rb
+++ lib/lrama/report/profile.rb
@@ -1,9 +1,14 @@
+# rbs_inline: enabled
 # frozen_string_literal: true
 
 module Lrama
   class Report
     module Profile
       # See "Call-stack Profiling Lrama" in README.md for how to use.
+      #
+      # @rbs enabled: bool
+      # @rbs &: -> void
+      # @rbs return: StackProf::result | void
       def self.report_profile(enabled)
         if enabled && require_stackprof
           StackProf.run(mode: :cpu, raw: true, out: 'tmp/stackprof-cpu-myapp.dump') do
@@ -14,6 +19,7 @@ module Lrama
         end
       end
 
+      # @rbs return: bool
       def self.require_stackprof
         require "stackprof"
         true
