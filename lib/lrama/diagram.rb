b34b458452ea4426e386eb7a8d4ddfe7f4d82367
ydah <t.yudai92@gmail.com>
2025-02-08 21:03:39 +0900

    Add RBS signatures for Diagram class and its methods

--- lib/lrama/diagram.rb
+++ lib/lrama/diagram.rb
@@ -1,3 +1,4 @@
+# rbs_inline: enabled
 # frozen_string_literal: true
 
 require "erb"
@@ -5,11 +6,13 @@ require "erb"
 module Lrama
   class Diagram
     class << self
+      # @rbs (IO out, Grammar grammar, String template_name) -> void
       def render(out:, grammar:, template_name: 'diagram/diagram.html')
         return unless require_railroad_diagrams
         new(out: out, grammar: grammar, template_name: template_name).render
       end
 
+      # @rbs () -> bool
       def require_railroad_diagrams
         require "railroad_diagrams"
         true
@@ -19,6 +22,7 @@ module Lrama
       end
     end
 
+    # @rbs (IO out, Grammar grammar, String template_name) -> void
     def initialize(out:, grammar:, template_name: 'diagram/diagram.html')
       @grammar = grammar
       @out = out
@@ -26,24 +30,29 @@ module Lrama
     end
 
     if ERB.instance_method(:initialize).parameters.last.first == :key
+      # @rbs (String input) -> ERB
       def self.erb(input)
         ERB.new(input, trim_mode: nil)
       end
     else
+      # @rbs override
       def self.erb(input)
         ERB.new(input, nil, nil)
       end
     end
 
+    # @rbs () -> void
     def render
       RailroadDiagrams::TextDiagram.set_formatting(RailroadDiagrams::TextDiagram::PARTS_UNICODE)
       @out << render_template(template_file)
     end
 
+    # @rbs () -> string
     def default_style
       RailroadDiagrams::Style::default_style
     end
 
+    # @rbs () -> string
     def diagrams
       result = +''
       @grammar.unique_rule_s_values.each do |s_value|
@@ -62,20 +71,24 @@ module Lrama
 
     private
 
+    # @rbs (String file) -> string
     def render_template(file)
       erb = self.class.erb(File.read(file))
       erb.filename = file
       erb.result_with_hash(output: self)
     end
 
+    # @rbs () -> string
     def template_dir
       File.expand_path('../../template', __dir__)
     end
 
+    # @rbs () -> string
     def template_file
       File.join(template_dir, @template_name)
     end
 
+    # @rbs (String name, RailroadDiagrams::Diagram diagram, String result) -> void
     def add_diagram(name, diagram, result)
       result << "\n<h2 class=\"diagram-header\">#{RailroadDiagrams.escape_html(name)}</h2>"
       diagram.write_svg(result.method(:<<))
