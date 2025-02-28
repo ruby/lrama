2fa4b9cc8c8526a747bb581ef3fce525afc5dcbd
ydah <t.yudai92@gmail.com>
2025-03-01 00:50:07 +0900

    Refactor inline rule handling to use inline? method for clarity

--- lib/lrama/grammar/parameterized/rule.rb
+++ lib/lrama/grammar/parameterized/rule.rb
@@ -10,7 +10,6 @@ module Lrama
         attr_reader :rhs #: Array[Rhs]
         attr_reader :required_parameters_count #: Integer
         attr_reader :tag #: Lexer::Token::Tag?
-        attr_reader :is_inline #: bool
 
         # @rbs (String name, Array[Lexer::Token] parameters, Array[Rhs] rhs, tag: Lexer::Token::Tag?, is_inline: bool) -> void
         def initialize(name, parameters, rhs, tag: nil, is_inline: false)
@@ -25,6 +24,11 @@ module Lrama
         # @rbs () -> String
         def to_s
           "#{@name}(#{@parameters.map(&:s_value).join(', ')})"
+        end
+
+        # @rbs () -> bool
+        def inline?
+          @is_inline
         end
       end
     end
