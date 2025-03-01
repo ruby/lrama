3439d54505d28ae24584c3cb002dbb2a81295962
ydah <t.yudai92@gmail.com>
2025-03-01 23:04:09 +0900

    Rewrite to rbs_inline style type definitions for Lrama::Grammar::PercentCode

--- lib/lrama/grammar/percent_code.rb
+++ lib/lrama/grammar/percent_code.rb
@@ -1,10 +1,20 @@
+# rbs_inline: enabled
 # frozen_string_literal: true
 
 module Lrama
   class Grammar
     class PercentCode
-      attr_reader :name, :code
+      # TODO: rbs-inline 0.10.0 doesn't support instance variables.
+      #       Move these type declarations above instance variable definitions, once it's supported.
+      #
+      # @rbs!
+      #   @name: String
+      #   @code: String
 
+      attr_reader :name #: String
+      attr_reader :code #: String
+
+      # @rbs (String name, String code) -> void
       def initialize(name, code)
         @name = name
         @code = code
