b85ad750474d7039fefe64018ceb2dca321a4a09
ydah <t.yudai92@gmail.com>
2025-03-18 01:34:19 +0900

    Fix wording related to inlines with parameters

--- spec/lrama/parser_spec.rb
+++ spec/lrama/parser_spec.rb
@@ -2613,8 +2613,8 @@ RSpec.describe Lrama::Parser do
         end
       end
 
-      context 'when inline with parameterized rule' do
-        let(:path) { "inlining/with_parameterized.y" }
+      context 'when inline with parameters' do
+        let(:path) { "inlining/with_parameters.y" }
 
         it "expands inlining rules" do
           expect(grammar.nterms.sort_by(&:number)).to match_symbols([
