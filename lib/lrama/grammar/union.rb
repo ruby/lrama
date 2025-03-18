23588129790f602fb93dbbe776bfc7b5ef8a9346
ydah <t.yudai92@gmail.com>
2025-03-18 23:09:54 +0900

    Rewrite to rbs_inline style type definitions for Lrama::Grammar::Union

--- lib/lrama/grammar/union.rb
+++ lib/lrama/grammar/union.rb
@@ -1,8 +1,16 @@
+# rbs_inline: enabled
 # frozen_string_literal: true
 
 module Lrama
   class Grammar
     class Union < Struct.new(:code, :lineno, keyword_init: true)
+      # @rbs!
+      #   attr_accessor code: Grammar::Code::NoReferenceCode
+      #   attr_accessor lineno: Integer
+      #
+      #   def initialize: (?code: Grammar::Code::NoReferenceCode, ?lineno: Integer) -> void
+
+      # @rbs () -> String
       def braces_less_code
         # Braces is already removed by lexer
         code.s_value
