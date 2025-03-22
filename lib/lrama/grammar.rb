6eadd8ebe6c0c79113a51b62bd77d8b3be882061
yui-knk <spiketeika@gmail.com>
2025-03-22 13:34:56 +0900

    Update type declarations
    Return values of these methods are not used.

--- lib/lrama/grammar.rb
+++ lib/lrama/grammar.rb
@@ -419,7 +419,7 @@ module Lrama
       end
     end
 
-    # @rbs () -> Array[Rule]
+    # @rbs () -> void
     def normalize_rules
       # Add $accept rule to the top of rules
       rule_builder = @rule_builders.first # : RuleBuilder
@@ -440,7 +440,7 @@ module Lrama
 
     # Collect symbols from rules
     #
-    # @rbs () -> Array[Lexer::Token]
+    # @rbs () -> void
     def collect_symbols
       @rules.flat_map(&:_rhs).each do |s|
         case s
