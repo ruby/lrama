41f394dc84ea64ef38d139fb471e35c0d8037624
yui-knk <spiketeika@gmail.com>
2025-07-28 09:34:37 +0900

    Use `action` type in `InadequacyAnnotation`

--- lib/lrama/state/inadequacy_annotation.rb
+++ lib/lrama/state/inadequacy_annotation.rb
@@ -5,14 +5,14 @@ module Lrama
   class State
     class InadequacyAnnotation
       # @rbs!
-      #   type action = Action::Shift | Action::Goto | Action::Reduce
+      #   type action = Action::Shift | Action::Reduce
 
       attr_accessor :state #: State
       attr_accessor :token #: Grammar::Symbol
-      attr_accessor :actions #: Array[Action::Shift | Action::Reduce]
-      attr_accessor :contribution_matrix #: Hash[Action::Shift | Action::Reduce, Hash[States::Item, bool]]
+      attr_accessor :actions #: Array[action]
+      attr_accessor :contribution_matrix #: Hash[action, Hash[States::Item, bool]]
 
-      # @rbs (State state, Grammar::Symbol token, Array[Action::Shift | Action::Reduce] actions, Hash[Action::Shift | Action::Reduce, Hash[States::Item, bool]] contribution_matrix) -> void
+      # @rbs (State state, Grammar::Symbol token, Array[action] actions, Hash[action, Hash[States::Item, bool]] contribution_matrix) -> void
       def initialize(state, token, actions, contribution_matrix)
         @state = state
         @token = token
@@ -25,7 +25,7 @@ module Lrama
         @contribution_matrix.any? {|action, contributions| !contributions.nil? && contributions[item] }
       end
 
-      # @rbs (Array[Hash[Action::Shift | Action::Reduce, Hash[States::Item, bool]]] another_matrixes) -> void
+      # @rbs (Array[Hash[action, Hash[States::Item, bool]]] another_matrixes) -> void
       def merge_matrix(another_matrixes)
         another_matrixes.each do |another_matrix|
           @contribution_matrix.merge!(another_matrix) {|action, contributions, another_contributions|
@@ -39,7 +39,7 @@ module Lrama
 
       # Definition 3.42 (dominant_contribution)
       #
-      # @rbs (State::lookahead_set lookaheads) -> Array[Action::Shift | Action::Reduce]?
+      # @rbs (State::lookahead_set lookaheads) -> Array[action]?
       def dominant_contribution(lookaheads)
         actions = @actions.select {|action|
           contribution_matrix[action].nil? || contribution_matrix[action].any? {|item, contributed| contributed && lookaheads[item].include?(@token) }
@@ -49,7 +49,7 @@ module Lrama
         resolve_conflict(actions)
       end
 
-      # @rbs (Array[Action::Shift | Action::Reduce] actions) -> Array[Action::Shift | Action::Reduce]
+      # @rbs (Array[action] actions) -> Array[action]
       def resolve_conflict(actions)
         # @type var shifts: Array[Action::Shift]
         # @type var reduces: Array[Action::Reduce]
