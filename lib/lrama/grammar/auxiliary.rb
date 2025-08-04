efba6ff94a550b8d4b4b8183c2dad0d1b6f57178
yui-knk <spiketeika@gmail.com>
2025-08-04 09:52:27 +0900

    Make `Auxiliary` to be plain object

--- lib/lrama/grammar/auxiliary.rb
+++ lib/lrama/grammar/auxiliary.rb
@@ -4,14 +4,11 @@
 module Lrama
   class Grammar
     # Grammar file information not used by States but by Output
-    class Auxiliary < Struct.new(:prologue_first_lineno, :prologue, :epilogue_first_lineno, :epilogue, keyword_init: true)
-      # @rbs!
-      #   attr_accessor prologue_first_lineno: Integer
-      #   attr_accessor prologue: String
-      #   attr_accessor epilogue_first_lineno: Integer
-      #   attr_accessor epilogue: String
-      #
-      #   def initialize: (?prologue_first_lineno: Integer, ?prologue: String, ?epilogue_first_lineno: Integer, ?epilogue: String) -> void
+    class Auxiliary
+      attr_accessor :prologue_first_lineno #: Integer?
+      attr_accessor :prologue #: String?
+      attr_accessor :epilogue_first_lineno #: Integer?
+      attr_accessor :epilogue #: String?
     end
   end
 end
