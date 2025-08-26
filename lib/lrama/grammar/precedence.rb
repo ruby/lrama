d96751a0fcf6026b1694feb9abeab96d605900cf
yui-knk <spiketeika@gmail.com>
2025-08-26 22:19:57 +0900

    Define `type_enum` for `Precedence#type`

--- lib/lrama/grammar/precedence.rb
+++ lib/lrama/grammar/precedence.rb
@@ -6,12 +6,14 @@ module Lrama
     class Precedence < Struct.new(:type, :precedence, :s_value, :lineno, keyword_init: true)
       include Comparable
       # @rbs!
-      #   attr_accessor type: ::Symbol
+      #   type type_enum = :left | :right | :nonassoc | :precedence
+      #
+      #   attr_accessor type: type_enum
       #   attr_accessor precedence: Integer
       #   attr_accessor s_value: String
       #   attr_accessor lineno: Integer
       #
-      #   def initialize: (?type: ::Symbol, ?precedence: Integer, ?s_value: ::String, ?lineno: Integer) -> void
+      #   def initialize: (?type: type_enum, ?precedence: Integer, ?s_value: ::String, ?lineno: Integer) -> void
 
       # @rbs (Precedence other) -> Integer
       def <=>(other)
